7e0019f034cffb80a2972c1c7c3a05e3bbf85422
Miklós Péch <miklos.pech@stud.uni-corvinus.hu>
2021-03-18 16:55:40 +0100

    commit03.18

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,30 @@
+﻿window.onload = () => {
+    console.warn(":)");
+    var faktorialisR = (n) => {
+        if (n === 0 || n === 1) {
+            return 1;
+
+        } else {
+            return n * faktorialisR(n - 1);
+        }
+    }
+    let hova = document.getElementById("pascal");
+    for (var s = 0; s < 10; s++) {
+
+        let sor = document.createElement("div");
+        sor.classList.add("sor")
+        hova.appendChild(sor)
+
+        for (var o = 0; o < s; o++) {
+            let szám = document.createElement("div");
+            szám.classList.add("doboz")
+            let c = faktorialisR(s - 1) / (faktorialisR(o) * faktorialisR(s - 1 - o))
+            szám.style.background = `rgb(${255-10*c},255,255)`
+            sor.appendChild(szám)
+           
+            szám.innerText = c
+
+        }
+
+    }
+}
---
+++ HajosTeszt/HajosTeszt/wwwroot/szorzo.js
@@ -0,0 +1,20 @@
+﻿window.onload = () => {
+    console.warn(":)");
+    let hova = document.getElementById("pascal");
+    for (var s = 0; s < 10; s++) {
+
+        let sor = document.createElement("div");
+        sor.classList.add("sor")
+        hova.appendChild(sor)
+
+        for (var o = 0; o < 10; o++) {
+            let szám = document.createElement("div");
+            szám.classList.add("doboz")
+            szám.style.background = `rgb(${255 / 10 * s},0,${255 / 10 * o})`
+            sor.appendChild(szám)
+            szám.innerText = (s + 1) * (o + 1)
+
+        }
+
+    }
+}
