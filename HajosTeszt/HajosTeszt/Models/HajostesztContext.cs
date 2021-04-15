b00e79602f86797405e4e7e676e0d5f7f55da897
Miklós Péch <miklos.pech@stud.uni-corvinus.hu>
2021-04-15 14:35:33 +0200

    commit0415

---
+++ HajosTeszt/HajosTeszt/Models/HajostesztContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace HajosTeszt.Models
+{
+    public partial class HajostesztContext : DbContext
+    {
+        public HajostesztContext()
+        {
+        }
+
+        public HajostesztContext(DbContextOptions<HajostesztContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Question> Questions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=sql.2rpi.hu,7000;Initial Catalog=Hajosteszt;User ID=hallgato;Password=Password123");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Hungarian_CI_AS");
+
+            modelBuilder.Entity<Question>(entity =>
+            {
+                entity.ToTable("Question");
+
+                entity.Property(e => e.QuestionId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("QuestionID");
+
+                entity.Property(e => e.Answer1).IsRequired();
+
+                entity.Property(e => e.Answer2).IsRequired();
+
+                entity.Property(e => e.Answer3).IsRequired();
+
+                entity.Property(e => e.Image)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.QuestionText).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
